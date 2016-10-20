79a94d95eae432740a33058f2c42af9d08d8accd
Hall, Kristina <HallK@AE.com>
2016-10-20 10:46:55 -0400

    rest-api-blueprint: Bring in rest-api blueprint for backend

--- app/models/story-chapter.js
+++ app/models/story-chapter.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+    story: DS.belongsTo('story'),
+    title: DS.attr('string'),
+    content: DS.attr('string')
 });
--- app/models/story.js
+++ app/models/story.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+    epic: DS.belongsTo('epic'),
+    title: DS.attr('string'),
+    author: DS.attr('string'),
+    description: DS.attr('string'),
+    chapters: DS.hasMany('story-chapter')
 });
